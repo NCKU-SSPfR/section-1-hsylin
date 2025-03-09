21682817334815ee7cb1f060ba60d200220ce7ad
hsing-yu-lin <hylin.cs@gmail.com>
2025-03-10 03:09:44 +0800

    refactor: DarkMaze commit

--- DarkMaze/backend/src/database/initialize.py
+++ DarkMaze/backend/src/database/initialize.py
@@ -17,13 +17,3 @@ def initialize():
     )
     """)
 
-    conn.commit()
-    conn.close()
-    conn = sqlite3.connect("game.db")
-    cursor = conn.cursor()
-    conn.commit()
-    conn.close()
-    conn = sqlite3.connect("game.db")
-    cursor = conn.cursor()
-    conn.commit()
-    conn.close()
