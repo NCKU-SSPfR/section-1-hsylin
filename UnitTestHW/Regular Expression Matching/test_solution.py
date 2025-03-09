370189dffb001844ef7edb92b69657a94f487d27
hsing-yu-lin <hylin.cs@gmail.com>
2025-03-09 17:45:46 +0800

    feat: UnitTestHW commit

--- UnitTestHW/Regular Expression Matching/test_solution.py
+++ UnitTestHW/Regular Expression Matching/test_solution.py
@@ -1,8 +1,9 @@
-import ...
-from ...
+import pytest
+from solution import Solution
 
 @pytest.fixture
-...
+def solution():
+    return Solution()
     
 testcases = [
     # s, p, expected_res
@@ -15,12 +16,10 @@ testcases = [
     ["aaa", "ab*a*c*a", True]
 ]
 
-@pytest.mark.parametrize([?], testcases)
-...
-    assert solution.isMatch(s, p) == expected
+@pytest.mark.parametrize("s, p, expected_res", testcases)
+def test_solution(solution, s, p, expected_res):
+    assert solution.isMatch(s, p) == expected_res
 
-"""
 @pytest.mark.xfail
 def test_broken_solution(solution):
-    assert solution.isMatch() == 
-"""
+    assert solution.isMatch("", "") == False
